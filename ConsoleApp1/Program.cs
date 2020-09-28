3751790dbec6960d445ba1cc130635c204c12356
nam-duc-tong <57309411+nam-duc-tong@users.noreply.github.com>
2020-09-28 23:47:37 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class BaseClass
+    {
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private Method");
+        }
+        public void PublicMethod()
+        {
+            Console.WriteLine("This Method Shared");
+        }
+    }
+    public class DerivedClass1 : BaseClass
+    {
+        public void DerivedClass1()
+        {
+            Console.WriteLine("Derived Class 1");
+        }
+
+    }
+
+    class Program : DerivedClass1
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.PublicMethod();
+            obj.DerivedClass1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
